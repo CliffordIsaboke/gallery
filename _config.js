4624ea9a9551ce6d2a852ff61eb13adfae2033ff
CliffordIsaboke <97104932+CliffordIsaboke@users.noreply.github.com>
2023-02-20 13:33:02 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,6 +2,5 @@ var config = {}
 
 config.mongoURI = {
  MONGODB_URI='mongodb+srv://clifford:Cmo0KNN84HuMuftS@isaboke.wdbovod.mongodb.net/?retryWrites=true&w=majority'
- PORT=8000
 }
 module.exports = config;
